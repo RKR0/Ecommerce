d28939b8507e6d914924a21a490fc323a32adf9e
RKR0 <114342990+RKR0@users.noreply.github.com>
2023-09-06 19:19:04 +0530

    Update Login.java

--- Ecommerce2/src/application/Login.java
+++ Ecommerce2/src/application/Login.java
@@ -1,5 +1,7 @@
+
 package application;
 
+import java.security.MessageDigest;
 import java.sql.ResultSet;
 
 public class Login {
@@ -14,7 +16,7 @@ public class Login {
 	ResultSet rs = dbcon.getQuertTable(loginQuery);
 	try {
 		if(rs.next())
-			return new Customers(rs.getInt("customer_id"),rs.getString("Name"),rs.getString("email"),rs.getString("mobile"));
+			return new Customers(rs.getInt("customer_id"),rs.getString("Name"),rs.getString("email"),rs.getString("mobile"),rs.getString("Address"),rs.getString("user_name"),rs.getString("password"));
 
 	}
 	catch(Exception e) {
@@ -22,5 +24,45 @@ public class Login {
 	}
 	return null;
 }
+	
+	public Customers customer1(int id) {
+	// search userName and password in customer_details table
+	String loginQuery = "select * From customer_details where  customer_id= "+id;
+	// connect with database
+	DbmsConnection dbcon = new DbmsConnection();
+	
+	ResultSet rs = dbcon.getQuertTable(loginQuery);
+	try {
+		if(rs.next())
+			return new Customers(rs.getInt("customer_id"),rs.getString("Name"),rs.getString("email"),rs.getString("mobile"),rs.getString("Address"),rs.getString("user_name"),rs.getString("password"));
+
+	}
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+	return null;
+}
+
+public String encrypt(String msg) {
+		
+		try {
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+			messageDigest.update(msg.getBytes());
+			byte bytearr[] = messageDigest.digest();
+			
+			StringBuilder str = new StringBuilder();
+			
+			for(byte b:bytearr) {
+				str.append(String.format("%02x",b));
+			}
+			return str.toString();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return "";
+	}
 
 }

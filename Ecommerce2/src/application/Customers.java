966e651859ecb11a3e849697bfbf1fdf9c5cd2b4
RKR0 <114342990+RKR0@users.noreply.github.com>
2023-09-06 19:16:13 +0530

    Update Customers.java

--- Ecommerce2/src/application/Customers.java
+++ Ecommerce2/src/application/Customers.java
@@ -1,18 +1,23 @@
 package application;
 
+import javafx.scene.layout.GridPane;
+
 public class Customers {
 
 	// Initialize customer details 
 	private int id;
-	private String name,email,mobile;
+	private String name,email,mobile,userName,password,address;
 	
 	
-	public Customers(int id, String name, String email, String mobile) {
+	public Customers(int id, String name, String email, String mobile,String address,String userName,String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.mobile = mobile;
+		this.userName = userName;
+		this.password = password;
+		this.address =address;
 	}
 
 
@@ -34,6 +39,28 @@ public class Customers {
 	public String getMobile() {
 		return mobile;
 	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	
+
+
+	
 
 	
 	
